5f5596fa02a634e8d78624a3a8c76b53e69c68e8
YvonneWu1999 <yvonnewu1324@gmail.com>
2021-02-11 01:24:36 +0800

    add about text and humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, lng, callback) => {
         } else if (body.error) {
             callback('Unable not find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
